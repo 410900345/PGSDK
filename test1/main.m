17a85c0a6954f078dc19637c74f29134c4737cae
yangshuo <410900345@qq.com>
2014-05-03 13:21:54 +0800

    Initial Commit

---
+++ test1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  test1
+//
+//  Created by yangshuo on 14-5-3.
+//  Copyright (c) 2014年 yangshuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
